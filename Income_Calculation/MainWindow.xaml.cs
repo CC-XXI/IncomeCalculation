ed6cf826ca210237462f5ee74ad2ce2edf99342f
CC-XXI <chanathip.chi@gmail.com>
2023-03-25 13:16:25 +0700

    Import file

---
+++ Income_Calculation/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Income_Calculation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double income = double.Parse(txtIncome.Text);
+            double expense = double.Parse(txtExpense.Text);
+            double goal = double.Parse(txtGoal.Text);
+
+            double result = goal / (income - expense);
+
+            txtResult.Text = result.ToString("N2");
+        }
+    }
+}
